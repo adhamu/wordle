c739c4ce1488cb9b7f18b139c1dbfa68d0fcf675
Amit Dhamu <dev@amitd.co>
2022-01-16 19:24:15 +0000

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,70 @@
 import * as React from 'react'
 
 const attempts = 6
-const wordLength = 5
-// const dictionary = ['break', 'broke', 'coral', 'loyal', 'panic', 'ferry']
-const targetWord = 'loyal'.toUpperCase()
+const dictionary = ['break', 'broke', 'coral', 'loyal', 'panic', 'ferry']
+const targetWord =
+  dictionary[Math.floor(Math.random() * dictionary.length)].toUpperCase()
+const wordLength = targetWord.length
 const defaultGuesses = new Array(attempts).fill({
   letters: [],
   status: [],
   disabled: false,
 })
 
+const getStyle = (
+  result: {
+    letter: string
+    exact: boolean
+  } | null
+) => {
+  if (result?.exact === true) {
+    return { backgroundColor: 'green', borderColor: 'green' }
+  }
+
+  if (result?.exact === false) {
+    return { backgroundColor: 'orange', borderColor: 'orange' }
+  }
+
+  return { backgroundColor: '#999', borderColor: '#999' }
+}
+
 const App = (): JSX.Element => {
   const [guesses, setGuesses] = React.useState(defaultGuesses)
   const [feedback, setFeedback] = React.useState('')
-  const [attemptsRemaining, setAttemptsRemaining] = React.useState(attempts)
+  const [currentAttempt, setCurrentAttempt] = React.useState(0)
 
-  const compareWithTarget = (word: string[]) => {
+  const isDisabled = (attempt: number) =>
+    guesses[attempt].disabled || currentAttempt !== attempt
+
+  const compareWithTarget = (letters: string[]) => {
     const results = []
     const targetSplit = targetWord.split('')
 
     for (let i = 0; i < wordLength; i += 1) {
-      results[i] = targetSplit[i] === word[i]
+      if (targetSplit[i] === letters[i]) {
+        results[i] = { letter: letters[i], exact: true }
+      } else if (
+        targetSplit.includes(letters[i]) &&
+        results.filter(f => f?.letter === letters[i]).length <
+          targetSplit.filter(r => r === letters[i]).length
+      ) {
+        results[i] = {
+          letter: letters[i],
+          exact: false,
+        }
+      } else {
+        results[i] = null
+      }
     }
 
     return results
   }
 
   const guessWord = (word: string, attempt: number) => {
+    if (guesses[attempt].letters.filter(Boolean).length !== wordLength) {
+      return
+    }
+
     setGuesses({
       ...guesses,
       [attempt]: {
@@ -40,47 +78,42 @@ const App = (): JSX.Element => {
       setFeedback(`${word} is the CORRECT word. Well done`)
     } else {
       setFeedback(`${word} is the incorrect word`)
-      setAttemptsRemaining(attemptsRemaining - 1)
+      setCurrentAttempt(currentAttempt + 1)
     }
   }
 
   return (
-    <>
-      Wordle!
+    <div className="container">
+      <h1>Wordle!</h1>
+      <hr />
       <p>{feedback}</p>
       <div>
         {Array.from({ length: attempts }, (_i, attempt) => (
-          <React.Fragment key={attempt}>
-            <p>Attempt #{attempt + 1}</p>
+          <section key={attempt}>
             {Array.from({ length: wordLength }, (_j, letter) => (
               <input
                 key={letter}
                 type="text"
                 maxLength={1}
                 tabIndex={Number([attempt + 1, letter].join(''))}
-                disabled={guesses[attempt].disabled}
+                disabled={isDisabled(attempt)}
                 style={
                   guesses[attempt].status.length
-                    ? {
-                        border: guesses[attempt].status[letter]
-                          ? '2px solid green'
-                          : '2px solid red',
-                      }
+                    ? getStyle(guesses[attempt].status[letter])
                     : {}
                 }
                 onKeyUp={e => {
                   const t = e.target as Partial<HTMLInputElement>
-                  t.value = t.value?.toUpperCase()
+                  t.value = t.value?.toUpperCase() || ''
+
+                  if (!/[A-Za-z]/.test(e.key)) {
+                    t.value = ''
+                  }
 
                   if (e.key === 'Backspace') {
                     ;(t.previousSibling as HTMLElement).focus()
                     ;(t.previousSibling as HTMLInputElement).value = ''
-                  }
-
-                  if (
-                    (e.key >= 'a' && e.key <= 'z') ||
-                    (e.key >= 'A' && e.key <= 'Z')
-                  ) {
+                  } else if (/[A-Za-z]/.test(e.key)) {
                     setGuesses({
                       ...guesses,
                       [attempt]: {
@@ -101,19 +134,18 @@ const App = (): JSX.Element => {
 
             <button
               type="button"
+              disabled={isDisabled(attempt)}
               tabIndex={Number([attempt + 1, wordLength].join(''))}
               onClick={() =>
-                guesses[attempt].letters.filter(Boolean).length ===
-                  wordLength &&
                 guessWord(guesses[attempt].letters.join(''), attempt)
               }
             >
-              Guess
+              ↵
             </button>
-          </React.Fragment>
+          </section>
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
